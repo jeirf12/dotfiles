ebab7ee414a7e1b76231a2821a7b6b22f3717d88
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2023-08-11 10:56:29 -0500

    update command generate codeQR

--- python/passCodeUrl.py
+++ python/passCodeUrl.py
@@ -1,15 +1,26 @@
 #! python
-import pyshorteners
-import qrcode
-import sys
+import qrcode, sys, cv2, pyshorteners,numpy as np
 
-if len(sys.argv) == 2:
-    url = sys.argv[1]
-    shortener = pyshorteners.Shortener()
-    short_url = shortener.dagd.short(url)
+params = sys.argv
+nameFile = "codigoQR"
 
-    img = qrcode.make(short_url)
-    img.save('urlQR.png')
-    # img.show()
+if len(params) > 1:
+    data = params[1]
+
+    if '-s' in params:
+        shortener = pyshorteners.Shortener()
+        short_url = shortener.dagd.short(data)
+        data = short_url
+
+    img = qrcode.make(data)
+
+    if '-g' in params:
+        print(f"foto guardada como: {nameFile}.png")
+        img.save(f'{nameFile}.png')
+    else:
+        imgprocess = np.array(img.convert('RGB'), dtype = np.uint8)
+        cv2.imshow(nameFile, imgprocess)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 else:
-    print("ERROR: Se introdujo (0) o mas de un (1) argumento\nEjemplo: python passCodeUrl.py www.google.com")
+    print("ERROR: Se introdujo (0) o mas de un (1) argumento\nEjemplo de uso: \n\t\tpython passCodeUrl.py www.google.com\n\t\t python passCodeUrl.py www.google.com -g\n\t\tpython passCodeUrl.py www.google.com -g -s")
